76f1afbdf6d41b25697ce6685b7511ec8c72f709
Aaroh Bhardwaj <103322439+aarohb1507@users.noreply.github.com>
2025-05-17 00:32:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 console.log("Welcome to TTT");
+let turn = 'X';
+let ting = new Audio("ting.mp3");
+const changeTurn = ()=>{
+  return turn === 'X' ? 0 : 'X';
+}
 
+// Game Logic 
+
+let boxes = document.getElementsByClassName('box');
+
+Array.from(boxes).forEach(element =>{
+  element.addEventListener('click',(e)=>{
+    let boxText = element.querySelector('.boxText');
+    if(boxText.innerHTML = " "){
+      boxText.innerHTML = turn;
+      changeTurn();
+      ting.play();
+      checkWin();
+    }else if(boxText.innerHTML != " "){
+      changeTurn();
+
+    }
+    
+  })
+})
